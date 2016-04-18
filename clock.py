4331f2cf5b5ef4a948c8fba600f7ac2e34715c00
genagain <ohta.g@husky.neu.edu>
2016-04-18 16:08:17 -0400

    removed schedule to see if I can do it dynamically

--- clock.py
+++ clock.py
@@ -3,14 +3,13 @@ from twilio.rest import TwilioRestClient
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=3)
-def timed_job():
+def text_gen():
   
   account_sid = "AC6e652d5e6bea5c4e494f9c7cb06be1a2"
   auth_token = "0d05da068ddc52eb021c930c27995ef0"
   client = TwilioRestClient(account_sid, auth_token)
 
   message = client.messages.create(to="+13479860720", from_="+16172022165", body="Hello Gen!")
-  print('This job is run every three minutes.')
+  print('This job is run every two minutes.')
 
 sched.start()
