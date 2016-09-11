7e07ffb0c45abb05292899e413a414ee8e9db7c7
genagain <ohta.g@husky.neu.edu>
2016-09-11 17:56:38 -0400

    added print statements in send email method

--- clock.py
+++ clock.py
@@ -6,12 +6,17 @@ from sendgrid.helpers.mail import *
 def send_email():
   try:
     sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+    print("created send grid api client")
     from_email = Email("ohta.g@husky.neu.edu")
+    print("created from email")
     subject = "Weekly update"
     # to_email = Email("j.kimani@northeastern.edu")
     to_email = Email("ohta.g@husky.neu.edu")
+    print("created to email")
     content = Content("text/plain", "Hello, Email!")
+    print("created content")
     mail = Mail(from_email, subject, to_email, content)
+    print("created mail")
     response = sg.client.mail.send.post(request_body=mail.get())
   except Exception as e:
     return e
