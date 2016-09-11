77865358710c9a2d0582f4d00983681154e37982
genagain <ohta.g@husky.neu.edu>
2016-09-11 15:40:02 -0400

    deleted twilio stuff

--- clock.py
+++ clock.py
@@ -1,16 +1,7 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from twilio.rest import TwilioRestClient
 import sendgrid
 import os
 from sendgrid.helpers.mail import *
-
-def text_gen():
-  account_sid = "AC6e652d5e6bea5c4e494f9c7cb06be1a2"
-  auth_token = "0d05da068ddc52eb021c930c27995ef0"
-  client = TwilioRestClient(account_sid, auth_token)
-
-  message = client.messages.create(to="+13479860720", from_="+16172022165", body="Hello Gen!")
-  print('This job is run every two minutes.')
 
 def send_email():
   sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
@@ -25,7 +16,7 @@ def send_email():
 try:
   sched = BackgroundScheduler()
   print("created background scheduler")
-  sched.add_job(send_email, 'cron', day_of_week=6, hour=18, minute=50)
+  sched.add_job(send_email, 'cron', day_of_week=6, hour=19, minute=30)
   print("added cron job")
   sched.start()
   print("started background scheduler")
