85e5343470984012b89d623006585b9e050ac95c
genagain <ohta.g@husky.neu.edu>
2016-09-12 19:43:50 -0400

    changed clock process to match heroku guide

--- clock.py
+++ clock.py
@@ -3,31 +3,22 @@ import sendgrid
 import os
 from sendgrid.helpers.mail import *
 
-def send_email():
-  try:
-    sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
-    print("created send grid api client")
-    from_email = Email("ohta.g@husky.neu.edu")
-    print("created from email")
-    subject = "Weekly update"
-    # to_email = Email("j.kimani@northeastern.edu")
-    to_email = Email("ohta.g@husky.neu.edu")
-    print("created to email")
-    content = Content("text/plain", "Hello, Email!")
-    print("created content")
-    mail = Mail(from_email, subject, to_email, content)
-    print("created mail")
-    response = sg.client.mail.send.post(request_body=mail.get())
-  except Exception as e:
-    return e
+sched = BlockingScheduler()
 
-try:
-  sched = BlockingScheduler()
-  print("created background scheduler")
-  sched.start()
-  print("started background scheduler")
-  sched.add_job(send_email, 'cron', day_of_week=6, hour=22, minute=20)
-  print("added cron job")
-except Exception as e:
-  print e.message
+@sched.scheduled_job('cron', day_of_week='tue', hour=0)
+def scheduled_job():
+  sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+  print("created send grid api client")
+  from_email = Email("ohta.g@husky.neu.edu")
+  print("created from email")
+  subject = "Weekly update"
+  # to_email = Email("j.kimani@northeastern.edu")
+  to_email = Email("ohta.g@husky.neu.edu")
+  print("created to email")
+  content = Content("text/plain", "Hello, Email!")
+  print("created content")
+  mail = Mail(from_email, subject, to_email, content)
+  print("created mail")
+  response = sg.client.mail.send.post(request_body=mail.get())
 
+sched.start()
