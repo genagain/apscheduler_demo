26493cf46ae241721324badb69c33641eb3b0bb5
genagain <ohta.g@husky.neu.edu>
2016-09-13 09:19:17 -0400

    sending actual email to professor

--- clock.py
+++ clock.py
@@ -5,27 +5,24 @@ from sendgrid.helpers.mail import *
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='tue', hour=9, minute=10)
+@sched.scheduled_job('cron', day_of_week='mon', hour=20, minute=13)
 def scheduled_job():
   sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
   print("created send grid api client")
   from_email = Email("ohta.g@husky.neu.edu")
   print("created from email")
   subject = "Weekly update"
-  # to_email = Email("j.kimani@northeastern.edu")
-  to_email = Email("ohta.g@husky.neu.edu")
+  to_email = Email("j.kimani@northeastern.edu")
   print("created to email")
   email_content = """
-  <html>
   Hi Professor Kimani,
 
   Here is a link to the GoogleDoc about our capstone progress.
 
-  <a href="https://docs.google.com/document/d/1FR2Vqhe9YctfCxOrWRoOO2HKIJjEpJp-CmSBV190vwE/edit?usp=sharing"> Weekly Progress </a>
+  https://docs.google.com/document/d/1FR2Vqhe9YctfCxOrWRoOO2HKIJjEpJp-CmSBV190vwE/edit?usp=sharing
 
   Best,
   Gen
-  </html>
   """
   content = Content("text/plain", email_content)
   print("created content")
